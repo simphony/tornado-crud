cda7f59520818b8561271da3d7ba4be89f4d4eb5
Stefano Borini <sborini@enthought.com>
2016-10-26 11:05:03 +0100

    Tests

--- tornadowebapi/tests/test_jsapi.py
+++ tornadowebapi/tests/test_jsapi.py
@@ -1,56 +1,19 @@
 from collections import OrderedDict
 from unittest import mock
 
-import tornadowebapi
-from tornadowebapi import registry, exceptions
 from tornadowebapi.http import httpstatus
+from tornadowebapi.registry import Registry
 from tornadowebapi.resource import Resource
 from tornadowebapi.tests.utils import AsyncHTTPTestCase
-from tornado import web, gen
-
-
-def prepare_side_effect(*args, **kwargs):
-    user = mock.Mock()
-    user.orm_user = mock.Mock()
-    args[0].current_user = user
+from tornado import web
 
 
 class Student(Resource):
+    pass
 
-    collection = OrderedDict()
-    id = 0
 
-    @gen.coroutine
-    def create(self, representation):
-        id = type(self).id
-        self.collection[str(id)] = representation
-        type(self).id += 1
-        return id
-
-    @gen.coroutine
-    def retrieve(self, identifier):
-        if identifier not in self.collection:
-            raise exceptions.NotFound()
-
-        return self.collection[identifier]
-
-    @gen.coroutine
-    def update(self, identifier, representation):
-        if identifier not in self.collection:
-            raise exceptions.NotFound()
-
-        self.collection[identifier] = representation
-
-    @gen.coroutine
-    def delete(self, identifier):
-        if identifier not in self.collection:
-            raise exceptions.NotFound()
-
-        del self.collection[identifier]
-
-    @gen.coroutine
-    def items(self):
-        return list(self.collection.keys())
+class Teacher(Resource):
+    pass
 
 
 class TestJSAPI(AsyncHTTPTestCase):
@@ -60,13 +23,18 @@ class TestJSAPI(AsyncHTTPTestCase):
         Student.id = 0
 
     def get_app(self):
-        handlers = tornadowebapi.api_handlers('/')
-        registry.registry.register(Student)
+        registry = Registry()
+        registry.register(Student)
+        registry.register(Teacher)
+        handlers = registry.api_handlers('/')
         app = web.Application(handlers=handlers)
         app.hub = mock.Mock()
         return app
 
-    def test_items(self):
+    def test_jsapi(self):
         res = self.fetch("/jsapi/v1/resources.js")
-        print(res.body)
         self.assertEqual(res.code, httpstatus.OK)
+        self.assertIn(b'"Student" : new Resource("students")',
+                      res.body)
+        self.assertIn(b'"Teacher" : new Resource("teachers")',
+                      res.body)
