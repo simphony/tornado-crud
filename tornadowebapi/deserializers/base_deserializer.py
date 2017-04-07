508cd8c63b4b0179bb4aa5b342506192c038796a
Stefano Borini <sborini@enthought.com>
2017-04-07 15:01:19 +0100

    Coverage

--- tornadowebapi/deserializers/base_deserializer.py
+++ tornadowebapi/deserializers/base_deserializer.py
@@ -5,6 +5,8 @@ class BaseDeserializer(metaclass=abc.ABCMeta):
     """Converts a dictionary of keys coming from
     the parser into an actual entity to pass to the
     resource handlers."""
+
     @abc.abstractmethod
     def deserialize_resource_data(self, data):
-        pass
+        """Deserializes the incoming data and return something that
+        the resource handler can accept"""
--- tornadowebapi/serializers/base_serializer.py
+++ tornadowebapi/serializers/base_serializer.py
@@ -8,18 +8,19 @@ class BaseSerializer(metaclass=abc.ABCMeta):
 
     This dictionary will then be passed to the renderer to be
     converted into something that is shown on the web.
+
+    NOTE: These methods will eventually accept a model object, rather than
+    the current parameters
     """
     @abc.abstractmethod
     def serialize_collection(self, collection_name, collection_items):
-        pass
+        """Serializes a collection of items"""
 
     @abc.abstractmethod
-    def serialize_exception(self, exception_representiation):
-        pass
+    def serialize_exception(self, exception_representation):
+        """Serializes an exception with a given representation"""
 
     @abc.abstractmethod
-    def serialize_resource(self,
-                           collection_name,
-                           identifier,
-                           resource_representation):
-        pass
+    def serialize_resource(self, collection_name, identifier, representation):
+        """Serializes a resource of a given collection name, identifier
+        and given representation"""
