7753b2dc4419803bfcf170d43c1bccfae4c55a3f
Stefano Borini <sborini@enthought.com>
2017-04-10 09:46:56 +0100

    Testing resource

---
+++ tornadowebapi/tests/test_resource.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tornadowebapi.resource import Resource
+from tornadowebapi.traitlets import Int, Unicode
+
+
+class Student(Resource):
+    name = Unicode()
+    age = Int()
+
+
+class TestResource(unittest.TestCase):
+    def test_instantiation(self):
+        s = Student()
+        self.assertEqual(s.collection_name(), "students")
