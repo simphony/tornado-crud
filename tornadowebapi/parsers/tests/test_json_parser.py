4d68c8e3c4b459edd0b7375bc8915c0b19c0724d
Stefano Borini <sborini@enthought.com>
2017-04-10 11:17:02 +0100

    In-out test

--- tornadowebapi/parsers/tests/test_json_parser.py
+++ tornadowebapi/parsers/tests/test_json_parser.py
@@ -1,6 +1,7 @@
 import unittest
 
 from tornadowebapi.parsers import JSONParser
+from tornadowebapi.renderers import JSONRenderer
 
 
 class TestJSONParser(unittest.TestCase):
@@ -8,3 +9,10 @@ class TestJSONParser(unittest.TestCase):
         parser = JSONParser()
         self.assertEqual(parser.parse("{}"), {})
         self.assertEqual(parser.parse(None), None)
+
+    def test_parser_renderer(self):
+        parser = JSONParser()
+        renderer = JSONRenderer()
+
+        for entity in [{}, None]:
+            self.assertEqual(entity, parser.parse(renderer.render(entity)))
