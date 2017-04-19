998ece6bd8de77c38b4ecf012d400c41e19bf7a4
Stefano Borini <sborini@enthought.com>
2017-04-19 09:21:15 +0100

    More tests and better handling of type

--- tornadowebapi/items_response.py
+++ tornadowebapi/items_response.py
@@ -1,3 +1,6 @@
+import copy
+
+from tornadowebapi.resource import Resource
 from traitlets import HasTraits, List, Int, Type
 
 
@@ -16,19 +19,50 @@ class ItemsResponse(HasTraits):
     #: The total number of items available.
     total = Int(0, min=0)
 
-    #: The type to check for the items. None means any type
-    _type = Type(allow_none=True)
+    #: The type to check for the items. None means any type.
+    _type = Type(klass=Resource, allow_none=True)
 
     def __init__(self, type, **kwargs):
+        """Instantiates the ItemsResponse for a specific type
+        of items.
+
+        Parameters
+        ----------
+        type: Resource or None
+            A Resource
+        """
         self._type = type
         super().__init__(**kwargs)
 
     def set(self, lst, offset=None, total=None):
-        self.items = lst[:]
+        """Sets the content of the object from a list.
+
+        Parameters
+        ----------
+        lst: list
+            The list of the response. It will be shallow copied into
+            self.items
+
+        offset: int or None
+            THe offset of the data, for partial responses. If None, it will be
+            zero by default
+
+        total: int or None
+            the total number of items, for a partial response. If None, it will
+            default to the length of the passed list.
+
+        Raises
+        ------
+        TypeError
+            If the type of the elements in the list do not match
+            with the declared type.
+        """
+        # Do a shallow copy without compromising the trait checks.
+        self.items = copy.copy(lst)
         self._check_list_types(self.items)
 
         self.offset = 0 if offset is None else offset
-        self.total = len(lst) if total is None else total
+        self.total = len(self.items) if total is None else total
 
     def _check_list_types(self, l):
         """Checks the list types to verify if they are all
--- tornadowebapi/tests/test_items_response.py
+++ tornadowebapi/tests/test_items_response.py
@@ -1,20 +1,40 @@
 import unittest
 
 from tornadowebapi.items_response import ItemsResponse
+from tornadowebapi.tests.resource_handlers import Student
+from traitlets import TraitError
 
 
 class TestItemsResponse(unittest.TestCase):
     def test_set_from_list(self):
-        response = ItemsResponse(int)
+        response = ItemsResponse(Student)
 
-        response.set([1, 2, 3, 4])
-        self.assertEqual(response.items, [1, 2, 3, 4])
+        items = [Student("1"),
+                 Student("2"),
+                 Student("3"),
+                 Student("4")]
+        response.set(items)
+        self.assertEqual(response.items, items)
         self.assertEqual(response.offset, 0)
         self.assertEqual(response.total, 4)
 
         with self.assertRaises(TypeError):
             response.set(["A", "B"])
 
-        response.set([1, 2, 3, 4], 3, 8)
+        response.set(items, 3, 8)
         self.assertEqual(response.offset, 3)
         self.assertEqual(response.total, 8)
+
+    def test_no_type(self):
+        response = ItemsResponse(None)
+
+        response.set([1, 2, 3, 4])
+        response.set(["A", "B"])
+
+        self.assertEqual(response.items, ["A", "B"])
+
+    def test_not_a_list(self):
+        response = ItemsResponse(None)
+
+        with self.assertRaises(TraitError):
+            response.set("hello")
