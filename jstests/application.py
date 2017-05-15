6058d9fa2e50ede39ebd987bac07a4cdb594d061
Stefano Borini <sborini@enthought.com>
2017-05-15 23:48:12 +0100

    Fixed selenium

--- jstests/application.py
+++ jstests/application.py
@@ -3,15 +3,15 @@ from tornado import web
 import tornado.ioloop
 
 from tornadowebapi.registry import Registry
-from tornadowebapi.tests.resource_handlers import StudentHandler
-from tornadowebapi.tests.resource_handlers import ServerInfoHandler
+from tornadowebapi.tests.resource_handlers import StudentModelConn
+from tornadowebapi.tests.resource_handlers import ServerInfoModelConn
 
 
 class Application(web.Application):
     def __init__(self):
         self.reg = Registry()
-        self.reg.register(StudentHandler)
-        self.reg.register(ServerInfoHandler)
+        self.reg.register(StudentModelConn)
+        self.reg.register(ServerInfoModelConn)
         handlers = self.reg.api_handlers('/')
         base_path = os.path.dirname(os.path.abspath(__file__))
         handlers += [('/(.*)', web.StaticFileHandler, {'path': base_path})]
